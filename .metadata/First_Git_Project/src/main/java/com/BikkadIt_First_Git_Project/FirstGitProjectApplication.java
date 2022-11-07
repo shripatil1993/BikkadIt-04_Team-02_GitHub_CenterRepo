fac5edc5c65e7ea45379e748ffc84faa67343dd1
shripatil1993 <117431987+shripatil1993@users.noreply.github.com>
2022-11-07 12:12:02 +0530

    team o2 new project

---
+++ .metadata/First_Git_Project/src/main/java/com/BikkadIt_First_Git_Project/FirstGitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIt_First_Git_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGitProjectApplication.class, args);
+	}
+
+}
---
+++ .metadata/First_Git_Project/src/test/java/com/BikkadIt_First_Git_Project/FirstGitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIt_First_Git_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstGitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
