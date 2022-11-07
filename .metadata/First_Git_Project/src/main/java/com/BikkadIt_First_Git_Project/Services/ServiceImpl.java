c64583ed36be67ef92df8c46ffe612f184543e9a
sarangdayalwar <dayalwarsarang123@gmail.com>
2022-11-07 15:22:22 +0530

    serviceImpl added

---
+++ .metadata/First_Git_Project/src/main/java/com/BikkadIt_First_Git_Project/Services/ServiceImpl.java
@@ -0,0 +1,11 @@
+package com.BikkadIt_First_Git_Project.Services;
+
+public class ServiceImpl implements ServiceI {
+
+	@Override
+	public int UserData() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
