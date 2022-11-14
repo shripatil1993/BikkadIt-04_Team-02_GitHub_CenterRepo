286be7db989ef29d51695425052ceedd7b57daed
alkarathod07 <alkarathod.rathod1@gmail.com>
2022-11-14 12:49:01 +0530

    save method added

---
+++ .metadata/First_Git_Project/src/main/java/com/BikkadIt_First_Git_Project/saveController/saveController.java
@@ -0,0 +1,10 @@
+package com.BikkadIt_First_Git_Project.saveController;
+
+public class saveController {
+	public String savem() {
+	System.out.println("save method completed");
+		return null;
+		
+	}
+
+}
