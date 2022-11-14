3bb94e176ce295f732dc447f05838eaa87c12f75
AnmolPalle <anmolpalle9@gmail.com>
2022-11-14 14:39:35 +0530

    Development under progress

---
+++ .metadata/First_Git_Project/src/main/java/com/BikkadIt_First_Git_Project/Anmol/Developer8/AnimalKingdom.java
@@ -0,0 +1,12 @@
+package com.BikkadIt_First_Git_Project.Anmol.Developer8;
+
+public class AnimalKingdom {
+	
+	public String lion() {
+		
+		System.out.println("King of Jungle");
+		return null;
+		
+	}
+
+}
